a2f7e6f2b149bb695b46c54538881ac95dcb1453
Al-AminJoy <alaminislam3555@gmail.com>
2022-02-08 23:13:20 +0600

    View & Text View Added in Adapter

--- app/src/main/java/com/alamin/databinding_kotlin/CustomAdapter.kt
+++ app/src/main/java/com/alamin/databinding_kotlin/CustomAdapter.kt
@@ -1,6 +1,8 @@
 package com.alamin.databinding_kotlin
 
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import coil.load
 
@@ -9,4 +11,16 @@ fun ImageView.loadImageFromUrl(photoUrl:String,percent:Int){
     if (percent>10){
         this.load(photoUrl);
     }
+}
+
+@BindingAdapter("setMyViewVisiblity")
+fun View.setMyViewVisiblity(points: Int){
+    if (points > 10){
+        this.visibility = View.INVISIBLE;
+    }
+}
+
+@BindingAdapter("setMyText")
+fun TextView.setMyText(text : String){
+    this.text = "First Name : "+text;
 }
